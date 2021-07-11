598d2cfe2da6ce533b70337d8744e0dc4d9faffc
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2021-07-11 19:46:27 +0800

    Updating questions

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Intern = require('./lib/Intern');
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const generatedHtmlFilePath = './dist/TeamProfile.html'
+
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
@@ -30,7 +32,15 @@ inquirer
         message: "Enter your employee ID",
 
     },
+    {
+        name: "email",
+        type: "input",
+        message: "What is your e-mail address?"
+
+    },
+    {
         name: ""
+    }
 
 ])
 
